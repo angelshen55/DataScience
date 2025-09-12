554a76010fa4d188edd22d0bb9689c0d67f79db6
angelshen55 <zshen575@connect.hkust-gz.edu.cn>
2025-09-12 14:08:55 +0800

    注释掉keystore android studio跑通

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,19 +43,22 @@ android {
     signingConfigs {
         // Create a variable called keystorePropertiesFile, and initialize it to your
         // keystore.properties file, in the rootProject folder.
-        val keystorePropertiesFile = rootProject.file("keystore.properties")
-
-        // Initialize a new Properties() object called keystoreProperties.
-        val keystoreProperties = Properties()
-
-        // Load your keystore.properties file into the keystoreProperties object.
-        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+//        val keystorePropertiesFile = rootProject.file("keystore.properties")
+//
+//        // Initialize a new Properties() object called keystoreProperties.
+//        val keystoreProperties = Properties()
+//
+//        // Load your keystore.properties file into the keystoreProperties object.
+//        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+//
+//        create("release") {
+//            keyAlias = keystoreProperties["keyAlias"] as String
+//            keyPassword = keystoreProperties["keyPassword"] as String
+//            storeFile = file(keystoreProperties["storeFile"] as String)
+//            storePassword = keystoreProperties["storePassword"] as String
+//        }
+        getByName("debug") {
+            // 使用默认的 debug 签名，无需额外配置
         }
     }
     namespace = "com.aisleron"
@@ -77,16 +80,16 @@ android {
     }
 
     buildTypes {
-        release {
-            isDebuggable = false
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.getByName("release")
-        }
+//        release {
+//            isDebuggable = false
+//            isMinifyEnabled = true
+//            isShrinkResources = true
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//            signingConfig = signingConfigs.getByName("release")
+//        }
 
         debug {
             isDebuggable = true
